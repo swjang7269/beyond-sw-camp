94b831b98d28fdd55762014da35864c3dd62d209
swjang7269 <azamiyuko7269@gmail.com>
2025-04-09 16:07:37 +0900

    vue 실습 예제

---
+++ lecture/15_vue/06_slot/src/components/01_component/MyComponent.vue
@@ -0,0 +1,16 @@
+<!-- 모든 컴포넌트를 App.vue에 붙이면 App.vue가 index.html에 부착된다. -->
+<template>
+    <div>
+        <h1 id="test">MyComponent</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    #test {
+        color: blue;
+    }
+</style>
